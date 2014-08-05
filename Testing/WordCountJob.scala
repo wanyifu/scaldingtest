d06d98952921af2b4b01796f3c2bb0b79bae3ea8
wanyifu <wanyifu2009@gmail.com>
2014-08-05 16:10:07 -0700

    First commit

---
+++ Testing/WordCountJob.scala
@@ -0,0 +1,8 @@
+import com.twitter.scalding._
+
+class WordCountJob(args : Args) extends Job(args) {
+  TextLine( args("input") )
+    .flatMap('line -> 'word) { line : String => line.split("""\s+""") }
+    .groupBy('word) { _.size }
+    .write( Tsv( args("output") ) )
+}
